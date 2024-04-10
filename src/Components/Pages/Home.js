f9a05e99d5a1ad19038da7dc06018ad6fe572b38
sidneyjfg <sidneyjunio191@gmail.com>
2024-04-09 21:23:01 -0300

    Adicionando pagina home

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,38 +1,40 @@
+import React, { useEffect } from 'react';
+import Typed from 'typed.js'; // Import Typed.js
+
+
 function Home() {
+    useEffect(() => {
+        const typed = new Typed('#element', {
+            strings: ['BackEnd Developer', 'FrontEnd Developer'],
+            typeSpeed: 50,
+            loop: true
+        });
+
+        // Optionally, clean up the Typed instance when the component unmounts
+        return () => typed.destroy();
+    }, []);
+
     return (
-        <div className="content-home">
-            <div class="card-client">
-                <div class="user-picture">
-                    <svg viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z"></path>
-                    </svg>
-                </div>
-                <p class="name-client"> Sidney Junio
-                    <span> Developer
-                    </span>
+        <div className='Home'>
+            <div className='home-content'>
+                <h3>Hello, It's me</h3>
+                <h1>Sidney Junio</h1>
+                <h3>And I'm a <span id='element'></span></h3>
+                <p>I'm a web designer with one year of experience.
+                    Experience in creating and applying website design, developing in BackEnd.
                 </p>
-                <div class="social-media">
-                    <a href="https://github.com/sidneyjfg/">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                            <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
-                        </svg>
-                        <span class="tooltip-social">GitHub</span>
-                    </a>
-                    <a href="https://www.linkedin.com/in/sidneyjunio/">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                            <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
-                        </svg>
-                        <span class="tooltip-social">LinkedIn</span>
-                    </a>
+                <div className='home-sci'>
+                    <a href='https://www.instagram.com/sidneyjfg/'><i className='bx bxl-instagram'></i></a>
+                    <a href='https://wa.link/sjpipp'><i className='bx bxl-whatsapp'></i></a>
+                    <a href='https://github.com/sidneyjfg'><i className='bx bxl-github'></i></a>
+                    <a href='https://www.linkedin.com/in/sidneyjunio/'><i className='bx bxl-linkedin'></i></a>
                 </div>
-            </div>
-            <div className="text-profile">
-                <h1>Olá meu nome é Sidney Junio</h1>
-                <p><p>Lorem ipsum dolor sit amet. Sed velit omnis ab natus nemo aut unde quisquam in earum labore qui consequuntur odit. At accusantium minima eum voluptatibus quis et maiores sunt et sequi temporibus aut beatae sint sit itaque aspernatur! Non quia galisum id similique quisquam in sequi dolores id expedita voluptatibus et ducimus officia non voluptas optio ea adipisci debitis. Rem sequi galisum ut asperiores pariatur eum illum praesentium ut rerum voluptatum vel quisquam dolor non ducimus maiores. </p><p>33 eaque earum est expedita voluptatibus ut libero iusto non iure iste et dignissimos nobis. Est sapiente autem eos earum perspiciatis vel galisum odit est saepe incidunt eos magnam voluptate qui reiciendis velit! Sit laboriosam voluptatem eos sapiente omnis ut laboriosam beatae in suscipit aliquid. </p><p>Et tempora sequi ut obcaecati sequi ut nesciunt autem. Vel omnis quisquam qui earum commodi sed minus soluta hic harum perspiciatis nam fugit minima ex amet amet ad numquam itaque. Et iusto ducimus et voluptatem modi est autem aliquam ut alias inventore sit sapiente quibusdam cum voluptates sint. </p>
-                </p>
+                <a href='../Portfolio/doc/CurriculoSidney.pdf' target='_blank' className='btn-more'><i className='bx bx-download'></i>
+                    Download Curriculum</a>
             </div>
         </div>
     )
+
 }
 
 export default Home
